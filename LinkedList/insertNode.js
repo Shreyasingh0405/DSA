25730a66d0c611b1e2cbdc9e75f4a0677fd37d8a
Shreya Singh <shreyasingh0405.52@gmail.com>
2024-10-21 17:31:36 +0530

    format

--- LinkedList/insertNode.js
+++ LinkedList/insertNode.js
@@ -1,32 +1,32 @@
 class linkedList {
   constructor(data) {
-      this.head = {
-          value: data,
-          next: null
-      }
-      this.tail = this.head
-      this.size = 1          // if want to count size
-  }
-  appendNode(nodeData){
-      let newNode={
-          value: nodeData,
-          next: null
-      }
-      this.tail.next=newNode
-      this.tail=newNode
-      this.size+=1
-  }
-  insertNode(index,value){
-    let counter=1;
-    let currentNode=this.head;
-    while(counter>index){
-      counter++
-      currentNode=currentNode.next
+    this.head = {
+      value: data,
+      next: null
     }
-    let nextNode=currentNode.next;
-    currentNode.next={
-      value:value,
-      next:nextNode
+    this.tail = this.head
+    this.size = 1          // if want to count size
+  }
+  appendNode(nodeData) {
+    let newNode = {
+      value: nodeData,
+      next: null
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.size += 1
+  }
+  insertNode(index, value) {
+    let counter = 1;
+    let currentNode = this.head;
+    while (counter > index) {
+      counter++
+      currentNode = currentNode.next
+    }
+    let nextNode = currentNode.next;
+    currentNode.next = {
+      value: value,
+      next: nextNode
     }
   }
 
@@ -38,6 +38,6 @@ list.appendNode(600)
 list.appendNode(700)
 list.appendNode(800)
 list.appendNode(100)
-list.insertNode(2,3000)
+list.insertNode(2, 3000)
 console.log(list)
 
